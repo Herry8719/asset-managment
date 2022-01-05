3c209dd78140d4569293977b5b089dfffbbbc37b
hariom vishwakarma <56312068+Herry8719@users.noreply.github.com>
2022-01-05 20:40:13 +0530

    Delete src directory

--- src/main/java/com/mangeassets/jpa/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mangeassets.jpa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mangeassets.jpa.model.Category;
-
-public interface CategoryRepository extends JpaRepository<Category,Integer> {
-
-}
